da66c4af86c106ce8f4eb65a607f34956fb46025
marcelo_macedo01@hotmail.com <marcelo_macedo01@hotmail.com>
2021-05-07 13:45:11 -0300

    Add InputText

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-void main(){
+
+void main() {
   runApp(MaterialApp(
-    home:Home() ,
-  )
-  );
+    home: Home(),
+  ));
 }
+
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
 
@@ -18,13 +20,40 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text( "Calculadora IMC"),
+        title: Text("Calculadora IMC"),
         centerTitle: true,
         actions: <Widget>[
           IconButton(
-            icon:  Icon(Icons.refresh) ,
+            icon: Icon(Icons.refresh),
             onPressed: () {},
           )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+
+          Icon(Icons.person_outline,size: 120.0,color: Colors.green,),
+          TextField(
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              icon: Icon(Icons.line_weight),
+              hintText: 'entre com seu Peso',
+              labelText: 'Peso *',
+            ),
+
+          ),
+      TextField(
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+        decoration: const InputDecoration(
+          icon: Icon(Icons.height),
+          hintText: 'entre com sua altura',
+          labelText: 'Altura *',
+        ),
+      ),
         ],
       ),
 
